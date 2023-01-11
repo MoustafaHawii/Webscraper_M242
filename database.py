e0898ab3885711e62f488eae8d1ea914deb214de
moustafahawi <hawim@bzz.ch>
2023-01-11 15:39:07 +0100

    create table

--- database.py
+++ database.py
@@ -2,17 +2,43 @@ import sqlite3
 from sqlite3 import Error
 
 def create_connection(db_file):
-    #erstellt eine Datenbankverbindung zu einer SQLite-Datenbank
     conn = None 
     try: 
         conn = sqlite3.connect(db_file)
-        print(sqlite3.version)
+        return conn
+
     except Error as e: 
         print(e)
-    finally:
-        if conn:
-            conn.close()
 
-            
+def create_table(conn, create_table_sql):
+    """ create a table from the create_table_sql statement
+    :param conn: Connection object
+    :param create_table_sql: a CREATE TABLE statement
+    :return:
+    """
+    try:
+        c = conn.cursor()
+        c.execute(create_table_sql)
+    except Error as e:
+        print(e)
+    
+def main():
+    database = r"C:\Users\svenh\OneDrive - Bildungszentrum Zürichsee\BZZ\Maurizi\Modul242\WebScrap\database_webscrape.db"
+
+    sql_create_main_table = """ CREATE TABLE IF NOT EXISTS projects (
+                                        id integer PRIMARY KEY,
+                                        store text,
+                                        datetime text,
+                                        price text
+                                    ); """
+                                    
+    conn = create_connection(database)
+
+    if conn is not None:
+        create_table(conn, sql_create_main_table)
+    else:
+        print("Error! can not creat the database connection")
+
+
 if __name__ == '__main__':
-    create_connection(r"C:\Users\svenh\OneDrive - Bildungszentrum Zürichsee\BZZ\Maurizi\Modul242\WebScrap\database_webscrape.db")
+    main()
