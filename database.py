e2e43004156a35f34e453cf74a36edb003064e89
moustafahawi <hawim@bzz.ch>
2023-01-14 15:28:00 +0100

    created class insertSQLite.py

--- database.py
+++ database.py
@@ -2,13 +2,18 @@ import sqlite3
 from sqlite3 import Error
 
 def create_connection(db_file):
-    conn = None 
-    try: 
+    """ create a database connection to the SQLite database
+        specified by db_file
+    :param db_file: database file
+    :return: Connection object or None
+    """
+    conn = None
+    try:
         conn = sqlite3.connect(db_file)
-        return conn
-
-    except Error as e: 
+    except Error as e:
         print(e)
+
+    return conn
 
 def create_table(conn, create_table_sql):
     """ create a table from the create_table_sql statement
