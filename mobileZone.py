2ee1c6838ec2763e905d522ab3f257d3f9327722
moustafahawi <hawim@bzz.ch>
2023-01-14 18:16:27 +0100

    finished insert

--- mobileZone.py
+++ mobileZone.py
@@ -1,6 +1,10 @@
 import requests
 from bs4 import BeautifulSoup
 import datetime 
+import sqlite3
+
+conn = sqlite3.connect('database.db')
+c = conn.cursor()
 
 def scrape():
     URL = "https://www.mobilezone.ch/de/mobilgeraete/handys/apple/iphone-14-pro-5g/deep-purple/1-tb/100010898"
@@ -9,12 +13,17 @@ def scrape():
     soup = BeautifulSoup(page.content, "html.parser")
     result = soup.find
 
-    prise_element = result("span", class_ = "mz-m-product-buy-box__wrapper__buy-box-inner__price-label")
+    price_element = result("span", class_ = "mz-m-product-buy-box__wrapper__buy-box-inner__price-label")
+    price = price_element.text 
     date_time = datetime.datetime.now()
+    name = "mobileZone"
 
-    print(prise_element.text)
+    print(price)
     print(date_time)
-    print("mobileZone")
+    print(name)
+
+    c.execute('''INSERT INTO projects VALUES(?,?,?)''',(name, date_time, price))
+    conn.commit()
 
 if __name__ == '__main__':
     scrape()
